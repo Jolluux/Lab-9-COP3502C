ab798abdf187e94c6299f21fa8486b0b2ea3df8d
Lucas <lucasaguirre0605@gmail.com>
2024-04-10 21:29:09 -0400

    Commit brother

--- Lab9.py
+++ Lab9.py
@@ -1,2 +1,10 @@
-print('Hi!')
-print('boobs and butt in my face! PLlease !~ ty brothree!1wodfhwoif')
+def encoder(string):
+    encoded_message = ''
+    for i in range(len(string)):
+        number = int(string[i])
+        number += 3
+        number = str(number)
+        encoded_message += number
+    return encoded_message
+
+print(encoder('1234566'))
