67efa972469e998d5d2b1b7ff80431e2e4183b51
Lucas <lucasaguirre0605@gmail.com>
2024-04-10 21:37:26 -0400

    Commit brother

--- Lab9.py
+++ Lab9.py
@@ -3,8 +3,29 @@ def encoder(string):
     for i in range(len(string)):
         number = int(string[i])
         number += 3
+        if number > 9:
+            number -= 9
         number = str(number)
         encoded_message += number
     return encoded_message
 
-print(encoder('1234566'))
+if __name__ == '__main__':
+    string = ''
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit\n')
+        menu_selection = int(input('Please enter an option: '))
+        if menu_selection == 3:
+            break
+        elif menu_selection == 1:
+            string = input('Please enter your password to encode:')
+            string = encoder(string)
+            print('Your password has been encoded and stored!')
+        elif menu_selection == 2:
+            string = encoder(string)
+            decoded = decoder(string)
+            print(f'The encoded passcode is {string}, and the original password is {decoded}')
+
